e4119844848196611c31889b79e1273ac82545ef
Georgy Simonov <jirisimonov@gmail.com>
2022-09-21 15:08:47 +0300

    fix: fix mistakes

--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -2,9 +2,10 @@ function getWidth() {
     const petsImg = document.querySelector('.pets__image').clientWidth;
     const petsItems = document.querySelectorAll('.pets__item');
     petsItems.forEach(e => {
-        e.setAttribute("style" , "max-width:" + petsImg + "px");
+        e.setAttribute("style" , "max-width:" + (petsImg + 2) + "px");
     })
 }
+getWidth();
 window.onresize = function( event ) {
     getWidth()
 }
