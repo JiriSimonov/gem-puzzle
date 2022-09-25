43451243c847ee1c94fccc810a04715442025643
Georgy Simonov <jirisimonov@gmail.com>
2022-09-25 23:10:15 +0300

    feat: add burger

--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -1,11 +1,21 @@
+const petsImg = document?.querySelector('.pets__image').clientWidth;
+const petsItems = document.querySelectorAll('.pets__item');
+const burgerItem = document.querySelector('.burger');
 function getWidth() {
-    const petsImg = document.querySelector('.pets__image').clientWidth;
-    const petsItems = document.querySelectorAll('.pets__item');
     petsItems.forEach(e => {
         e.setAttribute("style" , "max-width:" + (petsImg + 2) + "px");
     })
 }
-getWidth();
-window.onresize = function( event ) {
-    getWidth()
+
+function toggleBurger() {
+    burgerItem.addEventListener('click', () => {
+        burgerItem.classList.toggle('is-open')
+        console.log('clicked')
+    })
 }
+
+getWidth();
+    window.onresize = function( event ) {
+    getWidth();
+}
+toggleBurger();
