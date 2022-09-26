b77823e841cf5959e5dac456ce6479f8ed65a279
Georgy Simonov <jirisimonov@gmail.com>
2022-09-26 15:18:15 +0300

    fix: rework pets section

--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -1,21 +1,26 @@
 const petsImg = document?.querySelector('.pets__image').clientWidth;
 const petsItems = document.querySelectorAll('.pets__item');
+const petsDescr = document.querySelectorAll('.pets__descr');
 const burgerItem = document.querySelector('.burger');
-function getWidth() {
+/*function getWidth() {
     petsItems.forEach(e => {
         e.setAttribute("style" , "max-width:" + (petsImg + 2) + "px");
     })
+    petsDescr.forEach(e => {
+        e.setAttribute("style" , "max-width:" + (petsImg + 2) + "px");
+    })
 }
-
+*/
 function toggleBurger() {
     burgerItem.addEventListener('click', () => {
         burgerItem.classList.toggle('is-open')
         console.log('clicked')
     })
 }
-
+/*
 getWidth();
-    window.onresize = function( event ) {
+window.onresize = function( event ) {
     getWidth();
 }
+ */
 toggleBurger();
