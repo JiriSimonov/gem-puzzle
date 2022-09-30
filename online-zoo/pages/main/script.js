00749a4121d3d73199ca3b549dd7f255d0cecb73
Georgy Simonov <jirisimonov@gmail.com>
2022-09-30 17:42:12 +0300

    feat: add modal

--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -1,9 +1,16 @@
 const burgerItem = document.querySelector('.burger');
-function toggleBurger() {
-    burgerItem.addEventListener('click', () => {
-        burgerItem.classList.toggle('is-open')
-        console.log('clicked')
-    })
-}
-
-toggleBurger();
+const testimonialsBtn = document.querySelector('.btn-modal');
+const modalOverlay = document.querySelector('.modal__overlay');
+const modalContent = document.querySelector('.modal__content');
+const modalClose = document.querySelector('.modal__close');
+testimonialsBtn.addEventListener('click', () => {
+    modalOverlay.classList.add('modal__overlay--visible');
+    modalContent.classList.add('modal__content--visible');
+})
+modalClose.addEventListener('click', () => {
+    modalOverlay.classList.remove('modal__overlay--visible');
+    modalContent.classList.remove('modal__content--visible');
+})
+burgerItem.addEventListener('click', () => {
+    burgerItem.classList.toggle('is-open')
+})
