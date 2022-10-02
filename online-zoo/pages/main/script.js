e785f2370dcbc3d6374a30b6597188acbf7a730e
Georgy Simonov <jirisimonov@gmail.com>
2022-10-02 03:15:08 +0300

    feat: add @media 640

--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -1,10 +1,18 @@
+
+
 const burgerItem = document.querySelector('.burger');
-const burgerBtn = document.querySelector('.burger--orange');
 const testimonialsBtn = document.querySelector('.btn-modal');
 const modalOverlay = document.querySelector('.modal__overlay');
 const modalContent = document.querySelector('.modal__content');
 const modalClose = document.querySelector('.modal__close');
 const menu = document.querySelector('.nav');
+
+modalOverlay.addEventListener('click', (e) => {
+   if (e.target == modalOverlay) {
+       modalOverlay.classList.remove('modal__overlay--visible');
+       modalContent.classList.remove('modal__content--visible');
+   }
+})
 testimonialsBtn.addEventListener('click', () => {
     modalOverlay.classList.add('modal__overlay--visible');
     modalContent.classList.add('modal__content--visible');
