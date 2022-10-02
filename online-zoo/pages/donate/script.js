02edcff9f58319f6e13163b8bc5248679188caac
Georgy Simonov <jirisimonov@gmail.com>
2022-10-02 03:14:15 +0300

    feat: add @media 640

--- online-zoo/pages/donate/script.js
+++ online-zoo/pages/donate/script.js
@@ -1,9 +1,43 @@
 const line = document.querySelector('.donation__line');
 let items = document.querySelectorAll('.donation__item');
 const buttons = document.querySelectorAll('.donation__btn, .donation__button');
+const lineBtns = document.querySelectorAll('.donation__button');
 const burgerItem = document.querySelector('.burger');
-const burgerBtn = document.querySelector('.burger--orange');
 const menu = document.querySelector('.nav');
+const input = document.querySelector('.donation__input');
+let values = document.querySelectorAll('.donation__total');
+let count = 0;
+
+input.addEventListener('input', () => {
+    let curVal = input.value;
+    const countItems = document.querySelectorAll('.donation__total');
+    items.forEach(e => {
+        e.classList.remove('active');
+        values.forEach(e => {
+            e.classList.remove('current')
+        });
+    });
+    buttons.forEach(e => {
+        e.classList.remove('active');
+        e.classList.remove('target');
+        values.forEach(e => {
+            e.classList.remove('current')
+        });
+    });
+    countItems.forEach(e => {
+       if (e.textContent === curVal) {
+           e.classList.add('current');
+       }
+    });
+    lineBtns.forEach(e => {
+        if (e.textContent === curVal) {
+            lineBtns.forEach(e => {
+                e.parentNode.classList.remove('target');
+            })
+            e.parentNode.classList.add('target');
+        }
+    });
+});
 
 burgerItem.addEventListener('click', () => {
     burgerItem.classList.toggle('is-open');
@@ -50,10 +84,10 @@ function getActive() {
                 e.classList.remove('current');
             })
             countItems[i].classList.add('current');
-        })
+            count = parseInt(countItems[i].textContent);
+            input.value = count;
+        });
     });
-
-
 }
 
 function getMinMax() {
