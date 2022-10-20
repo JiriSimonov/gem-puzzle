1e627af4384f8b339f89018e09d908b792bb73a7
JiriSimonov <jirisimonov@gmail.com>
2022-10-20 20:39:10 +0300

    feat: add Score obj

--- utils/state.js
+++ utils/state.js
@@ -7,3 +7,76 @@
     isStartTimer: null,
     currentMaxtrix: {},
 }
+
+export const Score = {
+    first: {
+        position: '',
+        name: '',
+        moves: '',
+        minutes: '',
+        seconds: ''
+    },
+    second: {
+        position: '',
+        name: '',
+        moves: '',
+        minutes: '',
+        seconds: ''
+    },
+    thrird: {
+        position: '',
+        name: '',
+        moves: '',
+        minutes: '',
+        seconds: ''
+    },
+    fourth: {
+        position: '',
+        name: '',
+        moves: '',
+        minutes: '',
+        seconds: ''
+    },
+    fifth: {
+        position: '',
+        name: '',
+        moves: '',
+        minutes: '',
+        seconds: ''
+    },
+    sixth: {
+        position: '',
+        name: '',
+        moves: '',
+        minutes: '',
+        seconds: ''
+    },
+    seventh: {
+        position: '',
+        name: '',
+        moves: '',
+        minutes: '',
+        seconds: ''
+    },
+    eighth: {
+        position: '',
+        name: '',
+        moves: '',
+        minutes: '',
+        seconds: ''
+    },
+    ninth: {
+        position: '',
+        name: '',
+        moves: '',
+        minutes: '',
+        seconds: ''
+    },
+    tenth: {
+        position: '',
+        name: '',
+        moves: '',
+        minutes: '',
+        seconds: ''
+    }
+}
