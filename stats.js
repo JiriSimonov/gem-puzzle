660c044639fdb149f89f0d418cc0594c6f829dfb
JiriSimonov <103247465+JiriSimonov@users.noreply.github.com>
2022-10-21 21:25:39 +0300

    Delete stats.js

--- stats.js
+++
@@ -1,9 +0,0 @@
-import { createElement } from './utils/createElement.js';
-
-export const statsPanel = createElement({tag: 'div', eClass: 'stats-panel'});
-const statsWrapper = createElement({tag: 'div', eClass: 'stats-panel__wrapper', parent: statsPanel});
-const statsMoves = createElement({tag: 'div', eClass: 'stats-panel__moves', parent: statsWrapper, inner: 'Moves:'});
-export const statsMovesCounter = createElement({tag: 'div', eClass: 'stats-panel__moves-counter', parent: statsWrapper, inner: '0'});
-const statsTimer = createElement({tag: 'div', eClass: 'stats-panel__time', parent: statsWrapper, inner: 'Time:'});
-export const statsTimerCounter = createElement({tag: 'div', eClass: 'stats-panel__timer-counter', parent: statsWrapper, inner: '00'});
-export const statsTimerCounterSeconds = createElement({tag: 'div', eClass: 'stats-panel__timer-counter_seconds', parent: statsWrapper, inner: '00'});
