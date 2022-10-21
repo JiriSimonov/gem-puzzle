74badcaac76f7dad6f7c9164ad6322eb6f6ec2d5
JiriSimonov <103247465+JiriSimonov@users.noreply.github.com>
2022-10-21 21:24:57 +0300

    Delete utils directory

--- utils/createElementArr.js
+++
@@ -1,7 +0,0 @@
-export function createElementsArr({arrLength, callback, parent}) {
-    const arr = new Array(arrLength).fill(0).map(callback);
-    arr.forEach((e) => {
-        parent.appendChild(e);
-    });
-    return arr;
- }
