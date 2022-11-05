42f15809bc5010b8657a4b31e5412ac553bf60bd
JiriSimonov <jirisimonov@gmail.com>
2022-11-05 10:26:00 +0300

    feat: add header

---
+++ components/header.js
@@ -0,0 +1,26 @@
+import { createElement } from "../utils/createElement.js";
+
+export function createHeader(score) {
+    const header = createElement({ 
+        tag: "header",
+        eClass: "header" 
+    });
+    const headerWrapper = createElement({
+      eClass: "header__wrapper",
+      parent: header,
+    });
+    const headerLogo = createElement({
+      eClass: "header__logo",
+      parent: headerWrapper,
+    });
+    const headerLink = createElement({
+      tag: "a",
+      eClass: "header__link",
+      inner: "main",
+      parent: headerWrapper,
+      attr: { href: "/" },
+    });
+    headerWrapper.appendChild(score);
+    return header;
+}
+
---
+++ index.js
@@ -0,0 +1,12 @@
+import { createElement } from "./utils/createElement.js";
+import { createHeader } from "./components/header.js";
+
+export const app = document.getElementById('app');
+
+const headerScore = createElement({
+    eClass: "header__score",
+    inner: "Score: 0",
+});
+
+app.appendChild(createHeader(headerScore));
+
