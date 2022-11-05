3bbde79b1f5939648f80f7f48c25405561eefe3f
JiriSimonov <jirisimonov@gmail.com>
2022-11-05 08:51:25 +0300

    feat: add create elements function

---
+++ utils/createElements.js
@@ -0,0 +1,7 @@
+export function createElements({ arrLength, callback, parent }) {
+  const arr = new Array(arrLength).fill(0).map(callback);
+  arr.forEach((e) => {
+    parent.appendChild(e);
+  });
+  return arr;
+}
