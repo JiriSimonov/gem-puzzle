d8e018f3b6a112bc83a1e1949db15f2b662e0597
JiriSimonov <jirisimonov@gmail.com>
2022-11-10 06:08:52 +0300

    refactor: remove unused files

--- index.js
+++ index.js
@@ -1,27 +1,12 @@
 import { createElement } from "./utils/createElement.js";
-import { createElements } from "./utils/createElements.js";
 import { createHeader } from "./components/header.js";
 import { createFooter } from "./components/footer.js";
 import { createMain } from "./components/main.js";
-import BIRDS_DATA from "./data/data.js";
 
 export const app = document.getElementById("app");
 export const headerScore = createElement({
   eClass: "header__score",
   inner: "Счёт: 0",
-});
-const questionsWrapper = createElement({ eClass: "questions__wrapper" });
-const questionsArr = createElements({
-  arrLength: BIRDS_DATA.length,
-  parent: questionsWrapper,
-  callback: (_item, index) =>
-    createElement({
-      tag: "input",
-      eClass: "footer__link",
-      attr: { type: "checkbox" },
-      inner: `${BIRDS_DATA[index.name]}`,
-      parent: questionsWrapper,
-    }),
 });
 
 app.appendChild(createHeader(headerScore));
