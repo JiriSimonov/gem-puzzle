faeb561db3574608a99f47557e0e6ee73f5ce78f
JiriSimonov <jirisimonov@gmail.com>
2022-12-06 14:07:49 +0300

    feat: add inteface for NEWS-IP

---
+++ migration-newip-to-ts/src/inerfaces/interfaces.ts
@@ -0,0 +1,9 @@
+export default interface NewsProps {
+    id?: string;
+    name?: string;
+    description?: string;
+    url?: string;
+    category?: string;
+    language?: string;
+    country?: string;
+}
