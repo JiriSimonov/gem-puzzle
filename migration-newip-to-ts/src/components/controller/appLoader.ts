79351b27311bfcb07e65df7fca962f804e6fd1ee
JiriSimonov <jirisimonov@gmail.com>
2022-12-08 17:27:46 +0300

    feat: add new link

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.up.railway.app/', {
             apiKey: '8cc56851184349b7b541da66be86470d',
         });
     }
